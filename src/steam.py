498bb8cbb3dc4a2b16ab292b9ccda29b071c588b
JTnum4 <120874510+JTnum4@users.noreply.github.com>
2026-01-15 17:39:16 -0500

    Game Download Basic

--- src/steam.py
+++ src/steam.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 import os
+import httpx
 
 def main():
-    print("Hello World")
+    usersteamnumber = "XXX" # My profile to test.
+    gameidnumber = 613830 # Chrono Trigger to test
+    apikey = "ABC123"
 
+    # url = f"https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={gameidnumber}&key={apikey}&steamid={usersteamnumber}"
+    url = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apikey}&steamid={usersteamnumber}&format=json&include_appinfo=true"
+    response = httpx.get(url)
+    user_game_data = response.json()
+    print(user_game_data["response"]["games"][0]["name"])
+    
 if __name__ == "__main__":
-    main()
+   main()
